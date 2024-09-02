776765ced015c0b4892966dd8fe8a5392d351fb2
Voyageson <2582376763@qq.com>
2024-09-03 00:24:50 +0800

    chore(script): auto publish publish_release
    Add a Powershell script to automaticallly release to
    github release.

---
+++ publish_release.ps1
@@ -0,0 +1,21 @@
+param($TAG_NAME,$LATEST)
+$FILES_PATH = "./build/app/outputs/flutter-apk/*release.apk*"
+
+# create draft release
+gh release create $TAG_NAME --title "$TAG_NAME" --notes "" --draft
+
+# upload one by one
+Get-ChildItem -Path $FILES_PATH | ForEach-Object {
+    $file = $_.FullName
+    Write-Host "Uploading $file..."
+    gh release upload $TAG_NAME $file
+}
+
+# publish release
+if ($LATEST -eq $true) {
+    gh release edit $TAG_NAME --draft=false --latest
+} else {
+    gh release edit $TAG_NAME --draft=false --prerelease
+}
+
+Write-Host "All files uploaded successfully."

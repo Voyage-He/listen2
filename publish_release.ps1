6bdc7c5eb0faa9701bd4ecbb5ca352c9fc49da4b
Voyageson <2582376763@qq.com>
2024-09-07 19:12:55 +0800

    chore(publish): Add build cmd.

--- publish_release.ps1
+++ publish_release.ps1
@@ -1,5 +1,9 @@
 param($TAG_NAME,$LATEST)
 $FILES_PATH = "./build/app/outputs/flutter-apk/*release.apk*"
+$ErrorActionPreference = "Stop"
+
+# bulid
+flutter build apk --release --split-per-abi
 
 # create draft release
 gh release create $TAG_NAME --title "$TAG_NAME" --notes "" --draft

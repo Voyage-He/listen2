4fe4a36db8ca0e10ffa42889e4df15a3167127ee
Voyageson <2582376763@qq.com>
2024-09-16 01:06:29 +0800

    feat(view): Add modal to popup

--- lib/src/widget/popup.dart
+++ lib/src/widget/popup.dart
@@ -15,6 +15,12 @@ class PopupState extends State<Popup> with SingleTickerProviderStateMixin {
     super.initState();
     _animationController = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 300));
+
+    _animationController.addListener(() {
+      setState(() {
+        // 每次动画状态变化都会触发重建，可修改为AnimationBuilder，自动更新状态
+      });
+    });
   }
 
   open() {
@@ -35,19 +41,37 @@ class PopupState extends State<Popup> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return SlideTransition(
-      position: Tween<Offset>(
-        begin: const Offset(0, 3), // 从屏幕底部滑入
-        end: const Offset(0, 2),
-      ).animate(CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOut,
-      )),
+    print('adsa' + _animationController.status.toString());
+    if (_animationController.isDismissed) return const SizedBox.shrink();
+    return Stack(
+      children: [
+        modal(),
+        SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0, 3), // 从屏幕底部滑入
+            end: const Offset(0, 2),
+          ).animate(CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          )),
+          child: Container(
+            width: double.infinity,
+            height: 300,
+            // color: Colors.grey,
+            child: widget.child,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget modal() {
+    return GestureDetector(
+      onTap: () => close(),
       child: Container(
+        color: Color.fromARGB(150, 0, 0, 0),
         width: double.infinity,
-        height: 300,
-        color: Colors.grey,
-        child: widget.child,
+        height: double.infinity,
       ),
     );
   }
